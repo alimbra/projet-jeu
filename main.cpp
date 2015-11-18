166e3d7fd4231c4794bc3b2b40fe01fad01e8d86
ismail <dism2009@live.fr>
2015-11-18 15:17:45 +0100

    ajout la creation du jeu partie joueur 1

--- main.cpp
+++ main.cpp
@@ -113,4 +113,226 @@ void dessiner(DrawingWindow &w){
 		cx/2+wind-40,cy/2-300,
 		wind+cx/2-60,cy/2-315);
 	}
+	//creation du jeu
+
+	// Définition des variables et des constantes
+	float vx, vy , ax, ay, vr, xt, yt, angle, force;
+  	const float  k=0.005;
+  	const float g=9.81;
+  	bool victoire=false;
+
+	//le joueur 1 commence
+	bool joueur1=true;
+	int scorejoueur1=0;
+	int scorejoueur2=0;
+	// la position du boulet est calculee toutes les 0.0005 secondes
+  	float dt=0.0005;
+
+ 	//le vent est entre -20 et 20 et les dimensions de la colline sont aleatoires
+ 	int h=10;
+	int vrai_hauteur=h;
+	w.setColor("black");
+
+	//Affichage des dimensions de la colline, de la vitesse du vent dans la console
+  	cout<<endl<<"Hauteur de colline : "<<vrai_hauteur<<endl;
+  	cout<<"Largeur de la colline : "<<colline.getlongueur()<<endl;
+  	cout<<"Vitesse du vent : "<<wind<<endl;
+	while(victoire==false)
+	{
+
+	// Le joueur 1 joue
+
+	 if(joueur1==true)
+	 {
+
+	 // Lecture de la force et de l'angle de trajectoire du boulet
+
+   	  cout<<endl<<"Joueur 1 - Entrez une force : ";
+      cin>>force;
+      cout<<"Joueur 1 - Entrez un angle : ";
+      cin>>angle;
+      cout<<endl;
+	  angle =angle/180*M_PI;
+
+	  // Calcul de la vitesse initiale du boulet
+	  vx=ceil(force*cos(angle));
+      vy=ceil(force*sin(angle));
+
+	  // Coordonnées initiales du boulet
+      xt=chateau1.getCoordonneY().getX();
+      yt=chateau1.getCoordonneX().getY()-cy/2;
+      //yt=110;
+
+	  // Affichage du boulet à ses coordonées initiales
+ 	  w.setColor("black");
+  	  w.fillCircle(xt, (int)-yt+cy, 3);
+
+	  // Calcul et affichage de la trajectoire du boulet
+	  while((yt>0)&&(xt<cx+4))
+	  {
+  	   w.setColor("dodgerblue");
+  	   w.fillCircle((int)sqrt(xt*xt), (int)-yt+cy, 3);
+       vr=sqrt((vx-wind)*(vx-wind)+vy*vy);
+       ax=-k*vr*(vx-wind);
+  	   ay=-k*vr*vy-g;
+  	   vx=vx+ax*dt;
+   	   vy=vy+ay*dt;
+       xt=(xt+vx*dt);
+       yt=(yt+vy*dt);
+	   w.setColor("black");
+	   w.fillCircle((int)sqrt(xt*xt), (int)-yt+cy, 3);
+	   w.usleep(100);
+
+	   // Détection de la terre
+
+       if ((int)-yt+cy==cy-cy/8)
+	   {
+	    w.setColor("dodgerblue");
+	    w.fillCircle((int)sqrt(xt*xt), (int)-yt+cy, 10);
+	    yt=0;
+	   }
+
+	   //Detection de la colline
+       if(((w.getPointColor((int)sqrt(xt*xt)+5,
+		(int)-yt+cy+5)))==0xff008000)
+	   {
+	    w.setColor("dodgerblue");
+   	    w.fillCircle((int)sqrt(xt*xt),(int)-yt+cy,15);
+	    yt=0;
+	   }
+
+	   //Detection de la couleur darkslategrey des 2 chateaux
+	   if(((w.getPointColor((int)sqrt(xt*xt)+3,
+		(int)-yt+cy+3)))==0xff2f4f4f)
+	   {
+	     w.setColor("yellow");
+	     for(int k12=1;k12<=20;k12++){
+	      w.fillCircle((int)sqrt(xt*xt),(int)-yt+cy,k12);
+    	  w.msleep(100);
+	     }
+	     w.setColor("dodgerblue");
+	     w.fillCircle((int)sqrt(xt*xt),(int)-yt+cy,20);
+	     yt=0;
+	    if((int)sqrt(xt*xt)<cx/2)
+	    {
+	     w.setColor("black");
+	     w.drawText(cx/2-40,cy/2-280,"Suicide !");
+	     joueur1=false;
+	     victoire=true;
+	     int nombreR=0;
+	     cout<<"cliquez sur 1 pour recommencer ?";
+	     cin>>nombreR;
+	     if (nombreR==1)
+	     {
+	      dessiner(w);
+	     }
+	    }
+	    else
+	    {
+	     w.setColor("black");
+	     w.drawText(cx/2-40,cy/2-280,
+		"joueur 1 vous avez gagne!");
+	     victoire=true;
+	     int nombreR=0;
+	      cout<<"cliquez sur 1 pour recommencer ?";
+	      cin>>nombreR;
+	      if (nombreR==1)
+	      {
+	      	dessiner(w);
+	      }
+	    }
+	   }
+
+	   //Detection du mur du chateau
+	   if (((int)sqrt(xt*xt)+3==cx/2-275)&&
+		((int)-yt+3+cy>=cy-190))
+	   {
+	    w.setColor("yellow");
+	    for(int i3=1;i3<=20;i3++)
+	    {
+	     w.fillCircle((int)sqrt(xt*xt),
+	     (int)-yt+cy,i3);
+	     w.msleep(100);
+	    }
+	    w.setColor("dodgerblue");
+	    w.fillCircle((int)sqrt(xt*xt),
+	    (int)-yt+cy, 20);
+	    yt=0;
+
+	    if((int)sqrt(xt*xt)<cx/2)
+	    {
+	     w.setColor("black");
+	     w.drawText(cx/2-40,cy/2-280,"Suicide !");
+	     joueur1=false;
+         victoire=true;
+          int nombreR=0;
+	      cout<<"cliquez sur 1 pour recommencer ?";
+	      cin>>nombreR;
+	      if (nombreR==1)
+	      {
+	      	dessiner(w);
+	      }
+	    }
+	    else
+	    {
+	     w.drawText(cx/2-40,cy/2-280,
+		"joueur 1vous avez gagne !");
+	     joueur1=false;
+	     victoire=true;
+	     int nombreR=0;
+	      cout<<"cliquez sur 1 pour recommencer ?";
+	      cin>>nombreR;
+	      if (nombreR==1)
+	      {
+	      	dessiner(w);
+	      }}
+	    }
+
+	   //detection de la couleur dimgray des 2 chateaux
+	    if(((w.getPointColor((int)sqrt(xt*xt)+3,
+		(int)-yt+cy+3)))==0xff696969)
+	    {
+	     w.setColor("yellow");
+	     for(int i=1;i<=20;i++)
+	     {
+	      w.fillCircle((int)sqrt(xt*xt), (int)-yt+cy, i);
+	      w.msleep(100);
+	     }
+	     w.setColor("dodgerblue");
+	     w.fillCircle((int)sqrt(xt*xt), (int)-yt+cy, 20);
+	     yt=0;
+	   //detection de la couleur dimgray du chateau 1
+	     if((int)sqrt(xt*xt)<cx/2)
+	     {
+	      w.setColor("black");
+	      w.drawText(cx/2-20,cy/2-280,"Suicide !");
+	      joueur1=false;
+	      victoire=true;
+	      int nombreR=0;
+	      cout<<"cliquez sur 1 pour recommencer ?";
+	      cin>>nombreR;
+	      if (nombreR==1)
+	      {
+	      	dessiner(w);
+	      }
+	     }
+	     else
+	     {
+	      w.setColor("black");
+	      w.drawText(cx/2-40,cy/2-280,
+			"joueur 1 vous avez gagné");
+	      victoire=true;
+	       int nombreR=0;
+	      cout<<"cliquez sur 1 pour recommencer ?";
+	      cin>>nombreR;
+	      if (nombreR==1)
+	      {
+	      	dessiner(w);
+	      }
+	     }
+	    }
+	   }
+	  }
+ else
+	  {}
 }
